cfea481703da40b7cd1271c4feee26ccfbeeb63a
syniepolskyi <101953791+syniepolskyi@users.noreply.github.com>
2022-11-06 03:37:05 +0000

    Deploying to gh-pages from @ syniepolskyi/goit-react-hw-05-movies@879f2780e7a9a20fe3e9ae8a75c3c9e1d6498bb5 🚀

---
+++ static/js/Not-found-view.5cd87378.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies_2022=self.webpackChunkgoit_react_hw_05_movies_2022||[]).push([[523],{9661:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(8794),i="NotFoundView_wrapper__Udn-5",s=n(184);function c(){return(0,s.jsx)("div",{className:i,children:(0,s.jsx)("img",{src:r,width:"450",alt:"error"})})}},8794:function(e,t,n){e.exports=n.p+"static/media/no_results_found.98bc826327ced82ba54b.png"}}]);
+//# sourceMappingURL=Not-found-view.5cd87378.chunk.js.map
